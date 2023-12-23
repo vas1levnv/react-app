c4129a93b23d2f12bcd4953067019c687d72100f
vas1levnv <vas1levnv@yandex.ru>
2023-12-23 20:09:47 +0300

    Исправляет загрузку с постов с сервера

--- src/components/Pages/Posts/Posts.js
+++ src/components/Pages/Posts/Posts.js
@@ -6,34 +6,40 @@ const Posts = () => {
 	
 	const [posts, setPosts] = useState([])
 	const [error, setError] = useState(null)
-	
+	const [isLoading, setIsLoading] = useState(false)
 	const fetchPosts = async() => {
 		try {
-			const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+			setIsLoading(true)
+			const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+				params: {
+					_limit: 9
+				}
+			})
 			await new Promise((resolve) => setTimeout(resolve, 2000))
 			setPosts(response.data)
 		} catch(e) {
 			setError(e.message)
+		} finally {
+			setIsLoading(false)
 		}
-		
 	}
 	
 	useEffect(() => {
 		fetchPosts()
-	});
+	}, []);
 	
 	return (
 		<div className={s.posts}>
 			{error
-				?
-				<div className="error">{error}</div>
-				: <div className={s.postsList}>
-					{posts.map(el => <div key={el.id} className={s.postsItem}>
-						<div className={s.postsItemTitle}>{el.title}</div>
-						<div>{el.body}</div>
-					</div>)}
-				</div>}
-		
+				? <div className="error">{error}</div>
+				: isLoading
+					? <div className="error">Идет загрузка...</div>
+					: <div className={s.postsList}>
+						{posts.map(el => <div key={el.id} className={s.postsItem}>
+							<div className={s.postsItemTitle}>{el.title}</div>
+							<div>{el.body}</div>
+						</div>)}
+					</div>}
 		</div>
 	);
 };
